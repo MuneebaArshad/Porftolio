dc80661b6a37f134ef505923fa506c95d0550e0d
Muneeba Arshad <alisudhan69@gmail.com>
2024-11-24 02:20:58 +0500

    Portfolio-Nextjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      // Ignores ESLint errors during builds
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
+  
